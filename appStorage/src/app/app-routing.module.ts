3f9d3cffb2ef92fccc90fe893f8f80508e6ad160
AlejoQuiroz08 <steven.quiroz@epn.edu.ec>
2024-11-11 17:22:11 -0500

    Login

--- appStorage/src/app/app-routing.module.ts
+++ appStorage/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
 const routes: Routes = [
 	{
 		path: '',
-		loadChildren: () => import('./home/home.module').then((m) => m.HomePageModule),
+		loadChildren: () => import('./login/login.module').then((m) => m.LoginPageModule),
 		...canActivate(redirectLoggedInToHome)
 	},
 	{
